2d2293d93413940df0dcb8001b5abf8db2631808
Jenniferodd <jenniferodd@gmail.com>
2021-05-15 14:13:21 -0400

    Add project files.

---
+++ Test first class/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Test_first_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 25;
+            int y = 45;
+            int z = 70;
+
+            int max = 0;
+
+
+            if (x > max)
+            {
+                max = x;
+            }
+            if (y > max)
+            {
+                max = y;
+            }
+            if (z > max)
+            {
+                max = z;
+            }
+            Console.WriteLine(max);
+
+        }
+    }
+}
